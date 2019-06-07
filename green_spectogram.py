da6af615f2e22799d925e7c148a734db5bb0a099
Baheliya <51151857+Baheliya@users.noreply.github.com>
2019-06-08 00:25:41 +0530

    Add files via upload

---
+++ green_spectogram.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 22:34:21 2019
+
+@author: lshri
+"""
+
+import os
+import wave
+wav_file  =  'Recording6.wav'
+import pylab
+def graph_spectrogram(wav_file):
+    sound_info, frame_rate = get_wav_info(wav_file)
+    pylab.figure(num=None, figsize=(19, 12))
+    pylab.subplot(111)
+    pylab.title('spectrogram of %r' % wav_file)
+    pylab.specgram(sound_info, Fs=frame_rate)
+    pylab.savefig('spectrogram.png')
+def get_wav_info(wav_file):
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pylab.fromstring(frames, 'int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    return sound_info, frame_rate
+graph_spectrogram(wav_file)
+
+
